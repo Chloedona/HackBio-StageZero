b4fc2fa756d4af09780c21a3a3e6c1ae7d713e55
Chloedona <chloeanndonaghue@gmail.com>
2026-04-22 11:14:53 +0100

    Complete code

--- simple_code.py
+++ simple_code.py
@@ -1,2 +1,3 @@
-name = Chloe
-print("Hi, my name is", name) 
+name = "Chloe"
+print("Hello, my name is " + name + ".", "I am a Host Strain Development scientist at CPI.")
+print("My favourite gene is the 16S rRNA gene, because it is used for identifying and classifying bacteria, which is a big part of my work.")   
